425343b73bd8a25702815a74e0b81d00bf6caacb
Edgar Méndez <mdzredgar@gmail.com>
2023-03-12 18:49:48 -0500

    Agregado Tailwind al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="">
+      <header>
+        <h1 className='text-3xl font-bold underline'>Hola</h1>
+        <nav>Menu</nav>
+      </header>
+      <main>
+        <section>About me</section>
+        <section>Skills</section>
+        <section>Projects</section>
+        <section>Contact</section>
+      </main>
     </div>
   )
 }
