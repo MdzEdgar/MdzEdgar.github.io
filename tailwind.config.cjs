fd49ae28d6140492608af32adc0d623a19718ead
Edgar Méndez <mdzredgar@gmail.com>
2023-03-12 19:04:53 -0500

    Se agregan fuentes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'] ,
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+        'oxygen': ['Oxygen', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
